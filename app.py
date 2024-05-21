7b15dcc693ee300bf9df45eae4d01351a2c3c6a4
kishan-k9 <kk5005575@gmail.com>
2024-05-21 13:53:33 +0530

    latest 1

--- app.py
+++ app.py
@@ -72,8 +72,8 @@ def market():
     return render_template('market.html', supplement_image = list(supplement_info['supplement image']),
                            supplement_name = list(supplement_info['supplement name']), disease = list(disease_info['disease_name']), buy = list(supplement_info['buy link']))
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # app.run(
     # debug=True, passthrough_errors=True,
     # use_debugger=False, use_reloader=False)
-    app.run(debug=True)
+    # app.run(debug=True)
