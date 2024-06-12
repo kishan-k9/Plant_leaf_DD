9cdefbbd6a88ed34f47f85da50f58637c1eab388
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2024-06-12 16:01:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,108 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+import torchvision.transforms.functional as TF
+import torch
+import numpy as np
+import pandas as pd
+import torch.nn as nn
+from PIL import Image
+import re
+import io
+
+class CNN(nn.Module):
+    def __init__(self, K):
+        super(CNN, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d(2),
+        )
+        self.dense_layers = nn.Sequential(
+            nn.Dropout(0.4),
+            nn.Linear(50176, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+            nn.Linear(1024, K),
+        )
+
+    def forward(self, X):
+        out = self.conv_layers(X)
+        out = out.view(-1, 50176)
+        out = self.dense_layers(out)
+        return out
+
+targets_size = 39
+model = CNN(targets_size)
+model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
+model.eval()
+
+data = pd.read_csv("disease_info.csv", encoding="cp1252")
+
+app = FastAPI()
+origins = [
+    "http://localhost:3000",
+    "http://172.31.32.1:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    image_data = await file.read()
+    image = Image.open(io.BytesIO(image_data))
+    image = image.resize((224, 224))
+    
+    input_data = TF.to_tensor(image)
+    input_data = input_data.view((-1, 3, 224, 224))
+    
+    with torch.no_grad():
+        output = model(input_data)
+    
+    output = output.numpy()
+    index = np.argmax(output)
+    pred_csv = data["disease_name"][index]
+
+    original_name = file.filename.rsplit('.', 1)[0]
+    formatted_name = re.sub(r'_', ' ', original_name).title()
+
+    return JSONResponse(content={"Original": formatted_name, 
+                                 "prediction": pred_csv})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
